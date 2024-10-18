0640732e555045113eacd3c693a5daa01013e6ff
Rusty Trim <rustytrim11@gmail.com>
2024-10-17 22:51:34 -0400

    Create km_to_mi.py

---
+++ km_to_mi.py
@@ -0,0 +1,11 @@
+# km_to_mi.py
+#   A program that converts kilometers to miles
+#   By: Rusty Trim
+
+def main():
+    km = float(input("What is the distance in kilometers: "))
+    mi = km * 0.62
+
+    print(f"The distance is {mi} miles.")
+
+main()
