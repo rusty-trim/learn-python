a9ddc65f261e89f511bccf94de16c8b596c66225
Rusty Trim <rustytrim11@gmail.com>
2024-10-17 22:50:40 -0400

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# factorial.py
+#   Program to compute the factorial of a number
+#   Illustrates for loop with an accumulater
+
+def main():
+    n = int(input("Please enter a whole number to compute the factorial of: "))
+    fact = 1
+
+    for factor in range(n, 1, -1):
+        fact = fact * factor
+
+    print("The factorial of", n, "is", fact)
+
+main()
